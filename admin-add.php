a5dfce7b2a654924351accc8eb27c70b2820e889
Hadeel <113242182+HadeelAlanize@users.noreply.github.com>
2023-04-08 02:13:21 +0300

    Update admin-add.php

--- admin-add.php
+++ admin-add.php
@@ -39,16 +39,7 @@ if (isset($_POST['add-btn'])) {
 <!DOCTYPE html>
 
 <html>
-    <head>
-    <meta charset="UTF-8">
-	<meta http-equiv="X-UA-Cmpatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
-	<title> IVY Shop | Admin </title>
-	<!-- link to the css-->
-    <link rel="stylesheet" href="style.css">
-	<link href='http://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'> 
-	
-</head>
+
 <body>
         <section id="header">
         <a href="Home.php"> <img src="img/Logo_dark.png" class="logo" alt="" width="100" height="100"></a>
@@ -114,36 +105,7 @@ if (isset($_POST['add-btn'])) {
     </section>
 
 <footer class="section-p1">
-        <div class="col">
-            <img class="logo" src="img/Logo_dark.png" alt="" width="100" height="100">
-            <h4> Contact </h4>
-            <p> <strong> Address: </strong> King Faisal Ibn Abd Al Aziz, King Faysal University, Dammam 34212 </p>
-            <p> <strong> Phone: </strong> 013 333 3766 </p>
-            <p> <strong> Hours: </strong> sun-thu: 8:00am-8:00pm </p>
-            
-            
-            <div class="follow">
-                <h4>Follow Us</h4>
-                <div class="icon">
-                    <i class="fab fa-facebook-f"></i>
-                    <i class="fab fa-twitter"></i>
-                    <i class="fab fa-instagram"></i>
-                    <i class="fab fa-pinterest-p"></i>
-                    <i class="fab fa-youtube"></i>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="col">
-            <h4>About</h4>
-            <a href="Aboutt.html">About Us</a>
-            <a href="#">Delivery Information</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms & Conditions</a>
-            <a href="contact.html">Contact Us</a>
-        </div>
-
+       
         <div class="col">
             <h4>My Account</h4>
             <a href="account.php">Sign In</a>
@@ -153,19 +115,8 @@ if (isset($_POST['add-btn'])) {
             <a href="contact.html">Help</a>
         </div>
 
-            <div class="col install">
-            <h4> Install app </h4>
-                <p> from app store or google play </p>
-                <div class="row">
-                <img src="img/pay/Appstore.jpeg" alt="">
-                <img src="img/pay/play.jpg" alt="">
-                </div>
-                <p> Secured payment gateway </p>
-                <img src="img/pay/payment.png" alt="">
-            </div>
-            <div class="copyright">
-            <p> <center> © 2022, web based project - Class 3, Group 3</center>  </p>
-            </div>
+      
+          
         </footer>
         <script type="text/javascript">
                         function validateForm() {
