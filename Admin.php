a399ced2882b2219930f11fd3ecea0ab0b48c28d
Hadeel <113242182+HadeelAlanize@users.noreply.github.com>
2023-04-08 02:17:23 +0300

    Update Admin.php

--- Admin.php
+++ Admin.php
@@ -9,21 +9,7 @@ if(@$_SESSION['user']==""){
 <html>
     
     <body>
-        <section id="header">
-        <a href="Home.php"> <img src="img/Logo_dark.png" class="logo" alt="" width="100" height="100"></a>
-            
-        <div>
-            <ul id="navbar">
-            <li> <a href="Home.php">Home</a> </li>
-            <li> <a href="shop.php">Shop</a> </li>
-            <li> <a href="Aboutt.html">About</a></li>
-            <li> <a  href="contact.html">Contact</a> </li>
-            <li> <a class="active" href="Account.php">Account</a> </li>
-                 <li> <a href="FAQ.html">FAQ</a> </li>
-            <li> <a href="ncart.php"><i class='bx bxs-cart-alt'></i></a></li>
-            </ul>
-        </div>    
-        </section>
+       
       <script>
         var MainImg = document.getElementById("MainImg");
         var smallimg = document.getElementsByClassName("small-img");
