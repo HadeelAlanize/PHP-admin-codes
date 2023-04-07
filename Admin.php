4add2284ab106ce3d5fd35b3a97ddb037b21573e
Hadeel <113242182+HadeelAlanize@users.noreply.github.com>
2023-04-08 02:25:53 +0300

    Update Admin.php

--- Admin.php
+++ Admin.php
@@ -9,7 +9,19 @@ if(@$_SESSION['user']==""){
 <html>
     
     <body>
-       
+        <div class="col-2">
+            <div class="form-container">
+       <h4>Welcome Admin</h4>
+           <ul>
+               <li>  <a  href="admin-add.php"><button type="" class="btn">Add</button></a>  </li>
+                 <li> <a  href="admin-delete.php"><button type="" class="btn">Delete</button></a></li>
+                <li>  <a  href="admin-modify.php"> <button type="" class="btn">Modify</button></a></li>
+                <li>  <a  href="logout.php"> <button type="" class="btn">Logout</button></a></li>
+            </ul>
+                
+            </div>
+            </div>
+        
       <script>
         var MainImg = document.getElementById("MainImg");
         var smallimg = document.getElementsByClassName("small-img");
