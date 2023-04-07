d8d04261ae04e8bd599a7262268a1325e62a737e
Hadeel <113242182+HadeelAlanize@users.noreply.github.com>
2023-04-08 02:07:41 +0300

    Update Product_Details.php

--- Product_Details.php
+++ Product_Details.php
@@ -6,16 +6,6 @@ include "mysqli_connect.php";
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IVY Shop | Product Details</title>
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
-
-    <link rel="stylesheet" href="style.css">
-</head>
-
 <body>
 
     <section id="header">
@@ -128,119 +118,6 @@ if (isset($_POST["add_to_cart"])) {
 
 ?>
 
-    <section id="product1" class="section-p1">
-        <h2>Pet Friendly</h2>
-        <p><center>Our wide variety of pet-friendly plants are perfect for those who share their homes with curious canines and other furry friends. The Pet-Friendly Collection provides a non-toxic option to keep even the most mischievous pets happy and healthy.</p> <center>
-        <div class="pro-container">
-            <div class="pro">
-                <img src="img/Plants/Calathea'Beauty Star'.webp" alt="">
-                <div class="des">
-                    <span>Live Plant</span>
-                    <h5>Calathea'Beauty Star'</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h4>$20</h4>
-                </div>
-                <a href="ncart.html"><i class="fal fa-shopping-cart cart"></i></a>
-            </div>
-            <div class="pro">
-                <img src="img/Plants/Emina Fern.webp" alt="">
-                <div class="des">
-                    <span>Live Plant</span>
-                    <h5>Emina Fern.webp</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h4>$22</h4>
-                </div>
-                <a href="ncart.html"><i class="fal fa-shopping-cart cart"></i></a>
-            </div>
-            <div class="pro">
-                <img src="img/Plants/NematanthusCandyCorn.webp" alt="">
-                <div class="des">
-                    <span>Live Plant</span>
-                    <h5>Nematanthus Candy Corn</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h4>$22</h4>
-                </div>
-                <a href="ncart.html"><i class="fal fa-shopping-cart cart"></i></a>
-            </div>
-            <div class="pro">
-                <img src="img/Plants/Peperomia 'Frost.webp" alt="">
-                <div class="des">
-                    <span>Live Plant</span>
-                    <h5>Peperomia 'Frost</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h4>$18</h4>
-                </div>
-                <a href="ncart.html"><i class="fal fa-shopping-cart cart"></i></a>
-            </div>
-
-        </div>
-    </section>
-
-    <section id="newsletter" class="section-p1 section-m1">
-        <div class="newstext">
-            <h4>Sign Up For Newsletters</h4>
-            <p>Get E-mail updates about our latest shop and <span>special offers.</span> </p>
-        </div>
-        <div class="form">
-            <input type="text" placeholder="Your email address">
-            <button class="normal">Sign Up</button>
-        </div>
-    </section>
-
-    <footer class="section-p1">
-        <div class="col">
-            <img class="logo" src="img/Logo_dark.png" alt="" width="90" height="60">
-            <h4>Contact</h4>
-            <p><strong>Address: </strong> King Faisal In Abd Al Aziz, King Faysal University. Dammam 34212</p>
-            <p><strong>Phone:</strong> 013 333 3766</p>
-            <p><strong>Hours:</strong> sun-thu: 8:00am-8:00pm</p>
-            
-            
-            <div class="follow">
-                <h4>Follow Us</h4>
-                <div class="icon">
-                    <i class="fab fa-facebook-f"></i>
-                    <i class="fab fa-twitter"></i>
-                    <i class="fab fa-instagram"></i>
-                    <i class="fab fa-pinterest-p"></i>
-                    <i class="fab fa-youtube"></i>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <h4>About</h4>
-            <a href="Aboutt.html">About Us</a>
-            <a href="#">Delivery Information</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms & Conditions</a>
-            <a href="contact.html">Contact Us</a>
-        </div>
-
         <div class="col">
             <h4>My Account</h4>
             <a href="account.php">Sign In</a>
@@ -250,21 +127,9 @@ if (isset($_POST["add_to_cart"])) {
             <a href="contact.html">Help</a>
         </div>
 
-      
-        <div class="col install">
-            <h4>Install App</h4>
-            <p>From App Store or Google Play</p>
-            <div class="row">
-                <img src="img/pay/Appstore.jpeg" alt="">
-                <img src="img/pay/play.jpg" alt="">
-            </div>
-            
-        </div>
-
-                   <div class="copyright">
-            <p> <center> © 2022, web based project - Class 3, Group 3</center>  </p>
-            </div>
+     
     </footer>
+        
 
    
 </body>
